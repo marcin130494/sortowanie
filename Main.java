49779a0a60284275977c794c978aa86e6d785599
paweljanowski <paweljanowski6@gmail.com>
2018-01-31 18:24:39 +0100

    added info about 4th algorithm

--- Main.java
+++ Main.java
@@ -4,7 +4,7 @@ public class Main
 {
     public static void main (String [] args)
     {
-        System.out.println("Hello, this program sorts array with 3 algorithms: bubble, insertion and select.");
+        System.out.println("Hello, this program sorts array with 4 algorithms: bubble, insertion, select and quicksort.");
         int[] arr = new int[]{12, 11, 5, 71, 21, 2, 7};
     }
 }
