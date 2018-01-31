741627c938930acd1f9f17e43878ae12573dc91e
paweljanowski <paweljanowski6@gmail.com>
2018-01-31 18:23:08 +0100

    initial commit

---
+++ Main.java
@@ -0,0 +1,10 @@
+package sortingalgorithms;
+
+public class Main
+{
+    public static void main (String [] args)
+    {
+        System.out.println("Hello, this program sorts array with 3 algorithms: bubble, insertion and select.");
+        int[] arr = new int[]{12, 11, 5, 71, 21, 2, 7};
+    }
+}
